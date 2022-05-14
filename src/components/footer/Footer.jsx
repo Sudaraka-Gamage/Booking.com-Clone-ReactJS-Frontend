8a4b9512372465767f89cb1c1ad299764b77d721
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-05-14 12:45:49 +0530

    Footer Content

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,59 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+
+        <div className="fLists">
+            
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+
+            <ul className="fList">
+                <li className="fListItem">Homes</li>
+                <li className="fListItem">Apartments</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Villas</li>
+                <li className="fListItem">Hostels</li>
+                <li className="fListItem">B&Bs</li>
+            </ul>
+
+            <ul className="fList">
+                <li className="fListItem">Unique places to stay </li>
+                <li className="fListItem">All destinations</li>
+                <li className="fListItem">Discover</li>
+                <li className="fListItem">Seasonal and holiday deals</li>
+                <li className="fListItem">Unpacked: Travel articles</li>
+                <li className="fListItem">Travel Communities</li>
+            </ul>
+
+            <ul className="fList">
+                <li className="fListItem">Coronavirus (COVID-19) FAQs</li>
+                <li className="fListItem">About Booking.com</li>
+                <li className="fListItem">Customer Service help</li>
+                <li className="fListItem">Partner help</li>
+                <li className="fListItem">Sustainability</li>
+                <li className="fListItem">Press centre</li>
+            </ul>
+
+            <ul className="fList">
+                <li className="fListItem">Safety resource centre</li>
+                <li className="fListItem">Investor relations</li>
+                <li className="fListItem">Terms & Conditions</li>
+                <li className="fListItem">Partner dispute</li>
+                <li className="fListItem">Privacy & Cookie Statement</li>
+                <li className="fListItem">Corporate contact</li>
+            </ul>
+        </div>
+        <div className="fText">Copyright © 2020-2022 LankaBooking™. All rights reserved.</div>
+    </div>
+  )
+}
+
+export default Footer
